6154b197e7e66e92df5aebc452bf0cd846194f81
Dominik V. Salonen <hi@quad.moe>
2015-12-23 23:26:40 +0100

    Add more outputs

--- Hyozan/output.py
+++ Hyozan/output.py
@@ -4,8 +4,12 @@ import time
 def print_log(source, message):
   if source == "Main":
     print('\033[92m' + source + ': \033[0m' + message)
-  elif source == "Notice" or source == "Warning":
+  elif source == "Notice":
     print('\033[93m' + source + ': \033[0m' + message)
+  elif source == "Warning":
+    print('\033[91m' + source + ': \033[0m' + message)
+  elif source == "Web":
+    print('\033[95m' + source + ': \033[0m' + message)
   else:
     print('\033[94m' + source + ': \033[0m' + message)
 
